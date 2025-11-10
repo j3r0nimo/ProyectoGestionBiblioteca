723c1c446ae4b36a5ff46ef01c39f5d3cb9d8d12
marcos-1011 <marcos.diaz3088@gmail.com>
2025-11-10 20:41:13 -0300

    test 2 post fix de husky

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,5 +1,5 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 dotenv.config();
 
 let isconnected = false;
@@ -14,7 +14,7 @@ const connectDB = async () => {
       // 'throw new Error' immediately stops execution of the current function
       // This is correct: if uri doesn’t exist, execution should stop,
       // because there’s no point in trying to connect.
-      throw new Error("Hay un problema con la conexión a MongoDB.");
+      throw new Error('Hay un problema con la conexión a MongoDB.');
     }
 
     await mongoose.connect(uri);
@@ -24,8 +24,8 @@ const connectDB = async () => {
     // Here you catch any thrown error and log it to the console
     // 'console.error' do not stop the process, it just logs the error message to the console,
     // So you see what went wrong and then gracefully exit the process. That’s also fine.
-    console.error("Error de conección a MongoDB", err);
-    process.exit(1)
+    console.error('Error de conección a MongoDB', err);
+    process.exit(1);
   }
 };
 
